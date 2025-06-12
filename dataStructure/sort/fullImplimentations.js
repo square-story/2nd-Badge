a80956e2cde2044e7aee6dbaa27e2553a26b8a81
square-story <gibmepreo@gmail.com>
2025-06-12 20:26:26 +0530

    refactor: update sorting algorithms to use consistent variable naming and improve readability
    - Changed variable names from 'arr' to 'array' for clarity
    - Simplified bubbleSort, selectionSort, and insertionSort implementations
    - Removed unused quickSort and mergeSort functions
    - Updated console.log statements to reflect new variable names

--- dataStructure/sort/fullImplimentations.js
+++ dataStructure/sort/fullImplimentations.js
@@ -1,106 +1,52 @@
-let arr = [3, 4, 2, 5, 4, 2, 6, 3]
+const array = [1,4,6,2,4,623,3,2,566,23]
 
-const bubbleSort = (arr) => {
-    let swapped;
+const bubbleSort = (array)=>{
+    let swapped
     do {
         swapped = false
-        for (let i = 0; i < arr.length; i++) {
-            if (arr[i] > arr[i + 1]) {
-                [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]]
+        for(let i=0;i<array.length-1;i++){
+            if(array[i]>array[i+1]){
+                [array[i],array[i+1]] = [array[i+1],array[i]]
                 swapped = true
             }
         }
-    } while (swapped)
-    return arr
+    } while (swapped);
+
+    return array
 }
 
-console.log('the bubblesort array is :', bubbleSort(arr))
+console.log(bubbleSort(array))
 
-
-const selectionSort = (arr) => {
-    for (let i = 0; i < arr.length - 1; i++) {
-        let index = i
-        for (let j = i + 1; j < arr.length; j++) {
-            if (arr[index] > arr[j]) {
-                index = j
+const selectionSort = (array)=>{
+    for (let i = 0; i < array.length-1; i++) {
+        let minIndex = i;
+        for(let j=i+1;j<array.length;j++){
+            if(array[j]<array[minIndex]){
+                minIndex = j
             }
         }
-        if (i !== index) {
-            [arr[i], arr[index]] = [arr[index], arr[i]]
+        if(minIndex!==i){
+            [array[minIndex],array[i]] = [array[i],array[minIndex]]
         }
+        
     }
-    return arr
+
+    return array
 }
 
-console.log('the selectionSort is : ', selectionSort(arr))
+console.log(selectionSort(array))
 
-const insertionSort = (arr) => {
-    for (let i = 1; i < arr.length; i++) {
-        let j = i - 1
-        let value = arr[i]
-        while (j >= 0 && arr[j] > value) {
-            arr[j + 1] = arr[j]
+const insertionSort = (array) =>{
+    for(let i=1;i<array.length;i++){
+        let key = array[i]
+        let j = i-1
+        while(j>=0 && key<array[j]){
+            array[j+1] = array[j]
             j--
         }
-        arr[j + 1] = value
+        array[j+1] = key
     }
-    return arr
+    return array
 }
 
-console.log('the insertionSort is :', insertionSort(arr))
-
-
-const quickSort = (arr) => {
-    if (arr.length < 2) return arr
-    let pivot = arr[arr.length - 1]
-    let left = []
-    let right = []
-    for (let i = 0; i < arr.length - 1; i++) {
-        if (arr[i] < pivot) {
-            left.push(arr[i])
-        } else {
-            right.push(arr[i])
-        }
-    }
-    return [...quickSort(left), pivot, ...quickSort(right)]
-}
-
-console.log('the quickSort is :', quickSort(arr))
-
-
-const mergeSort = (arr) => {
-    if (arr.length < 2) return arr
-    let middle = Math.floor(arr.length / 2)
-    let left = arr.slice(0, middle)
-    let right = arr.slice(middle)
-    return merge(mergeSort(left), mergeSort(right))
-}
-
-const merge = (left, right) => {
-    let arr = []
-    while (left.length && right.length) {
-        if (left[0] < right[0]) {
-            arr.push(left.shift())
-        } else {
-            arr.push(right.shift())
-        }
-    }
-    return [...arr, ...left, ...right]
-}
-
-console.log('the mergeSort is :', mergeSort(arr))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(insertionSort(array))
